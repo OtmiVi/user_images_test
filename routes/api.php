32629a3aade59c759e5934f11b4e2aecbd551b46
Yura Kuziv <kyzivyra.3@gmail.com>
2023-08-31 11:58:43 +0300

    Update method

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('users', [UserController::class, 'index']);
+Route::get('users', [UserController::class, 'getUsers']);
 Route::post('users', [UserController::class, 'store']);
