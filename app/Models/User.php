fd62ff63ae8311c0d7ee4f52c0014130fcdcdae4
Yura Kuziv <kyzivyra.3@gmail.com>
2023-09-01 12:17:45 +0300

    Add fillable properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,11 @@ class User extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'city'
+    ];
+
     public function images()
     {
         return $this->belongsToMany(UserImage::class, 'user_user_image', 'user_id', 'user_image_id')
--- app/Models/UserImage.php
+++ app/Models/UserImage.php
@@ -9,6 +9,9 @@ class UserImage extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'image',
+    ];
     public function user()
     {
         return $this->belongsToMany(User::class, 'user_user_image', 'user_image_id', 'user_id')
